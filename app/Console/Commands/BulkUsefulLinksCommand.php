c9d13b26aee319b96ee14024fa72717624f5077d
Yusuf Demir <ysffdmr50@gmail.com>
2023-02-09 23:36:45 +0300

    fix

--- app/Console/Commands/BulkUsefulLinksCommand.php
+++ app/Console/Commands/BulkUsefulLinksCommand.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 
 use App\Models\UsefulLink;
 use Illuminate\Console\Command;
-use Termwind\Components\Dd;
 
 class BulkUsefulLinksCommand extends Command
 {
@@ -29,38 +28,38 @@ class BulkUsefulLinksCommand extends Command
      */
     public function handle()
     {
-//         - afetharita.com
-// Sosyal medyada paylaşılmış ihbarları harita üzerinde gösterir
+        //         - afetharita.com
+        // Sosyal medyada paylaşılmış ihbarları harita üzerinde gösterir
 
-// - afetbilgi.com
-// Barınma ve toplanma alanları, bağış yardım noktaları, önemli telefonlar gibi hayati bilgilerini derlenmiştir.
+        // - afetbilgi.com
+        // Barınma ve toplanma alanları, bağış yardım noktaları, önemli telefonlar gibi hayati bilgilerini derlenmiştir.
 
-// - beniyiyim.com
-// Deprem bölgesindeki insanların durumlarını diğer insanlara bildirme sitesi
+        // - beniyiyim.com
+        // Deprem bölgesindeki insanların durumlarını diğer insanlara bildirme sitesi
 
-// - depremyardim.com
-// Yardıma ihtiyacı olan depremzedelerin adreslerinin ayrıntılı bir şekilde girilen site
+        // - depremyardim.com
+        // Yardıma ihtiyacı olan depremzedelerin adreslerinin ayrıntılı bir şekilde girilen site
 
-// - yakinimibul.net
-// Ailesinden haber alamayan karın kontrol edilebileceği listelerin oluşturduğu site
+        // - yakinimibul.net
+        // Ailesinden haber alamayan karın kontrol edilebileceği listelerin oluşturduğu site
 
-// -deprem.io
-// Enkaz altında kalan kişilerin bilgilerinin girildiği ve bu bilgilerin dernek ve kurumlarla paylaşıldığı site
+        // -deprem.io
+        // Enkaz altında kalan kişilerin bilgilerinin girildiği ve bu bilgilerin dernek ve kurumlarla paylaşıldığı site
 
-// - stream.epctex.com
-// Enkaz altında kalanların bildirildiği tweetleri tek yerden takibini sağlayan site
+        // - stream.epctex.com
+        // Enkaz altında kalanların bildirildiği tweetleri tek yerden takibini sağlayan site
 
-// - deprem.basarsoft.com.tr
-// Tüm yardım taleplerini gösteren ve talep girebileceğiniz site
+        // - deprem.basarsoft.com.tr
+        // Tüm yardım taleplerini gösteren ve talep girebileceğiniz site
 
-// - depremihtiyac.com
-// Deprem bölgesindeki yardımları tek bir platformda toplayan site
+        // - depremihtiyac.com
+        // Deprem bölgesindeki yardımları tek bir platformda toplayan site
 
-// - yikilanbinalar.com.tr
-// Yıkılan binaların adres bilgilerinin ve durumlarının girildiği site
+        // - yikilanbinalar.com.tr
+        // Yıkılan binaların adres bilgilerinin ve durumlarının girildiği site
 
-// - enkazbildirim.org
-// Türkiye'nin güncel enkaz haritasını gösteren site
+        // - enkazbildirim.org
+        // Türkiye'nin güncel enkaz haritasını gösteren site
 
 
         $data = [
@@ -121,15 +120,14 @@ class BulkUsefulLinksCommand extends Command
             ],
             [
                 "title" => "Afad Acil Çağrı",
-                "url" => "https://play.google.com/store/apps/details?id=tr.gov.icisleri.afad",
+                "url" => "play.google.com/store/apps/details?id=tr.gov.icisleri.afad",
                 "description" => "T.C. İçişleri Bakanlığı Afet ve Acil Durum Yönetimi Başkanlığı Acil Çağrı mobil uygulaması. Toplanma alanlarını görüntüleyebilir ve afet durumunda acil çağrı başlatabilirsiniz."
             ]
-            ];
+        ];
 
         foreach ($data as $item) {
             UsefulLink::create($item);
         }
         dd('done');
-
     }
 }
