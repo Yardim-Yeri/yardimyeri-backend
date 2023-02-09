6ca9815c0049fcbcb54474e352d098a3ca1ce029
Ramazan Değirmenci <45834008+ramomen@users.noreply.github.com>
2023-02-09 18:32:40 +0000

    dd

---
+++ app/Http/Controllers/Pages/UsefulLinksController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsefulLinksController extends Controller
+{
+    public function index()
+    {
+        $links = \App\Models\UsefulLink::all();
+        return view('useful-links', compact('links'));
+
+        // return view('pages.useful-links.index');
+    }
+}
---
+++ resources/views/useful-links.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>Yararlı Linkler</h1>
+    <ul>
+        @foreach ($links as $link)
+            <li><a href="{{ $link->url }}">{{ $link->title }}</a></li>
+        @endforeach
+    </ul>
+@endsection
