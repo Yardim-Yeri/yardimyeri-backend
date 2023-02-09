42c708c5e65aaaeb4ae2efbc08f27717e6297be5
ramomen <rmdnmllr@gmail.com>
2023-02-09 22:34:06 +0300

    fixed useful-links blade

--- resources/views/useful-links.blade.php
+++ resources/views/useful-links.blade.php
@@ -2,9 +2,17 @@
 
 @section('content')
     <h1>Yararlı Linkler</h1>
-    <ul>
-        @foreach ($links as $link)
-            <li><a href="https://{{ $link->url }}">{{ $link->title }}</a></li>
-        @endforeach
-    </ul>
+    <div class="row align-item-center justify-content-center mt-4 pt-4">
+        <ol class="list-group list-group-numbered">
+            @foreach ($links as $link)
+
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+              <div class="ms-2 me-auto">
+                <div class="fw-bold"><a href="https://{{ $link->url }}">{{ $link->title }}</a></div>
+                {{ $link->description }}
+              </div>
+            </li>
+            @endforeach
+          </ol>
+    </div>
 @endsection
