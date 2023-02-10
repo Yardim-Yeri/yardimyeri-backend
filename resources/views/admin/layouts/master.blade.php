b7eb12831a3df8137e81536f7bab79ff03c61cb9
StarrkF <faymergen98@gmail.com>
2023-02-11 00:07:59 +0300

    demand pagination fixed

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -25,23 +25,9 @@
 
 <body>
 
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        <div class="navbar-nav">
-          <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="#">Sign out</a>
-          </div>
-        </div>
-      </header>
+    @include('admin.layouts.utils.navbar')
 
-    <div class="container-fluid">
-        <div class="row">
-            @include('admin.layouts.utils.navbar')
-        </div>
+    <div class="container-fluid py-5">
         <div class="m-5">
             @yield('admin-content')
         </div>
@@ -49,21 +35,11 @@
         
     </div>
     
-    
-    
-       
-  
-    <footer class="fixed-bottom">
-        <div class="copyright text-center border-top mt-5 py-4">
-            <span class="d-inline-block">Geri Bildirim: </span><a href="mailto:yardimyeri.info@gmail.com" target="_blank" class="d-inline-block">Mail Gönder</a>
-            <span class="ms-4">© 2023 YARDIMYERİ.COM</span>
-        </div>
-    </footer>
-
     @vite(['resources/js/admin.js'])
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     @yield('script')
     @stack('sc')
+
 
 </body>
 
