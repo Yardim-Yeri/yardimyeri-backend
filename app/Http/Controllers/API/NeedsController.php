aebf3cf1395a78a8aa627d5bac2647a27ec1d691
Yusuf Demir <ysffdmr50@gmail.com>
2023-02-11 21:16:12 +0300

    needs endpoint changes

--- app/Http/Controllers/API/NeedsController.php
+++ app/Http/Controllers/API/NeedsController.php
@@ -9,14 +9,21 @@ class NeedsController extends Controller
     public function index()
     {
         $needs = [];
+        $data = config('needs');
+
+        foreach ($data as $key => $item) {
+            $item_array = [];
+            $item_array['id'] = $key;
+            $item_array['label'] = $item;
+
+            $needs['result'][] = $item_array;
+        }
 
         try {
-            $needs['result'] = config('needs');
             $needs['success'] = true;
+            return $this->respondWithData($needs);
         } catch (\Throwable $th) {
             return $this->respondError('Error get needs');
         }
-
-        return $this->respondWithData($needs);
     }
 }
