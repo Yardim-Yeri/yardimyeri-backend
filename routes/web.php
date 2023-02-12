91d9d26ccc544575d74d1b1db8f32b25bef37e3a
Yusuf Demir <ysffdmr50@gmail.com>
2023-02-12 18:43:25 +0300

    admin changes

--- routes/web.php
+++ routes/web.php
@@ -26,22 +26,23 @@ Route::get('/yardim-talebim-var', [PageController::class, 'yardimTalebimVar'])->
 Route::get('/yardimda-bulunabilirim/{id?}', [PageController::class, 'yardimdaBulunabilirim'])->name('yardimda-bulunabilirim');
 Route::get('yararli-linkler', [UsefulLinksController::class, 'index'])->name('yararli-linkler');
 
-Route::get('login',[AuthController::class,'index'])->name('get.login');
-Route::post('login',[AuthController::class,'login'])->name('post.login');
-Route::get('logout',[AuthController::class,'logout'])->name('logout');
+Route::get('login', [AuthController::class, 'index'])->name('get.login');
+Route::post('login', [AuthController::class, 'login'])->name('post.login');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::group(['prefix'=>'admin','middleware'=>'auth'],function(){
-    Route::get('',function(){
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('', function () {
         return redirect()->route('get.admin-demands');
     });
-    Route::get('demands',[DemandController::class,'index'])->name('get.admin-demands');
-    Route::get('demands/{id}',[DemandController::class,'show'])->name('show.admin-demand');
-    Route::post('demands/{id}',[DemandController::class,'update'])->name('update.admin-demand');
-    Route::get('users',[UsersController::class,'index'])->name('get.admin-users');
-    Route::post('users',[AuthController::class,'register'])->name('store.admin-users');
-    Route::get('users/delete/{id}',[UsersController::class,'delete'])->name('delete.admin-users');
-    Route::get('useful-links',[LinkController::class,'index'])->name('get.useful-links');
-    Route::post('useful-links',[LinkController::class,'store'])->name('store.useful-links');
-    Route::post('useful-links/update/{id}',[LinkController::class,'update'])->name('update.useful-links');
-    Route::get('useful-links/delete/{id}',[LinkController::class,'delete'])->name('delete.useful-links');
+    Route::get('demands', [DemandController::class, 'index'])->name('get.admin-demands');
+    Route::get('demands/{id}', [DemandController::class, 'show'])->name('show.admin-demand');
+    Route::post('demands/{id}', [DemandController::class, 'update'])->name('update.admin-demand');
+    Route::post('demands/delete/{id}', [DemandController::class, 'destroy'])->name('delete.admin-demand');
+    Route::get('users', [UsersController::class, 'index'])->name('get.admin-users');
+    Route::post('users', [AuthController::class, 'register'])->name('store.admin-users');
+    Route::get('users/delete/{id}', [UsersController::class, 'delete'])->name('delete.admin-users');
+    Route::get('useful-links', [LinkController::class, 'index'])->name('get.useful-links');
+    Route::post('useful-links', [LinkController::class, 'store'])->name('store.useful-links');
+    Route::post('useful-links/update/{id}', [LinkController::class, 'update'])->name('update.useful-links');
+    Route::get('useful-links/delete/{id}', [LinkController::class, 'delete'])->name('delete.useful-links');
 });
